52f82c7dc2403bd130031ed38685b2c079c99864
ZyfiDev <jeltesijtsma22222@gmail.com>
2024-10-28 11:52:18 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,51 @@
+--ZYFIAC 
+--ZYFIAC 
+--ZYFIAC 
+--ZYFIAC 
+--ZYFIAC 
+
+
+Config = {}
+
+Config.Locale = 'en' --Choose from: nl or en
+
+Config.whitelist = {
+	'steam:steamid'
+}
+
+-- How many peds before we start deleting?
+Config.pedThreshold = 100
+
+--GENERAL
+Config.permission        = "admin" --Permission needed to use ban commands (mod-admin-superadmin)
+
+
+--WEBHOOK
+Config.Discord				= 'https://discord.gg/'
+Config.EnableDiscordLink = true -- only turn this on if you want link the log to a discord
+Config.webhookban        = "https://discordapp.com/api/webhooks/"
+Config.webhookunban      = "https://discordapp.com/api/webhooks/"
+Config.green             = 56108
+Config.grey              = 8421504
+Config.red               = 16711680
+Config.orange            = 16744192
+Config.blue              = 2061822
+Config.purple            = 11750815
+
+Config.CarsBL = {
+	"khanjali",
+	"example" -- if you wanne add new one type , and the last one no ,
+}
+
+Config.WeaponBL={
+	"WEAPON_RAILGUN",
+	"WEAPON_GARBAGEBAG",
+	"WEAPON_RPG",
+	"WEAPON_MINIGUN",
+	"WEAPON_PROXMINE"
+}
+
+Config.ObjectsBL={
+	"p_gasmask_s",
+	"prop_flamingo"
+}
